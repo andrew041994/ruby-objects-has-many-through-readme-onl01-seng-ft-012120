4b83d2106f4307eb22b2ed9ad562246f8cb67203
Alehandro Persaud <andrew041994@gmail.com>
2020-02-08 00:18:08 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class Customer
-  
+
+attr_accessor :name, :age
+    @@all = []
+    @@meals = []
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def new_meal(waiter, price, tip)
+      new_meal = Meal.new(waiter, self, price, tip)
+      @@meals << new_meal
+    end
+
+    def meals
+
+    end
+
+
 end
